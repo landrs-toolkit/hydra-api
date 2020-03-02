b5a4cf876b60521bc247eda9d0d11c25e2e4634c
James Sweet <james.sweet88@googlemail.com>
2020-03-02 11:16:26 -0500

    Replaced data store with SPARQL store to interact with fuseki

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express')
-const FlatMultiFileStore = require('rdf-store-fs/FlatMultiFileStore')
+const SPARQLStore = require('rdf-store-sparql')
 const hydraBox = require('hydra-box')
 const ResourceStore = require('./lib/ResourceStore')
 
 async function main () {
-  const store = new FlatMultiFileStore({
-    baseIRI: 'http://localhost:9000/',
-    path: 'store'
-  })
+  const store = new SPARQLStore(
+    'http://fuseki-geosparql:3030/ds',
+    {updateUrl: 'http://fuseki-geosparql:3030/ds/update'}
+  )
 
   const api = await hydraBox.Api.fromFile('api.ttl', {
     path: '/api',
